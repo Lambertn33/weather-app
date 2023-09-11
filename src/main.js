706d68e6b8c9e3830cf5d61c4552a9a5d4a14457
Lambertn33 <lambertgithub@gmail.com>
2023-09-11 10:33:13 +0200

    feat: add modal

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import BaseModal from './components/BaseModal.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.component('base-modal', BaseModal)
 
 app.use(router)
 
